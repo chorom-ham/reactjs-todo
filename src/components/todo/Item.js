10ea277b82e722d94ae5f9bd7aa417b0ae9c968c
arm-B <arm0258@naver.com>
2021-05-17 18:51:43 +0900

    add line-through when toggle is true

--- src/components/todo/Item.js
+++ src/components/todo/Item.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
+import React, { useState } from "react";
 
 // Hint: props로 id, text, onClick 등 (자유)
 export default function Item({ id, text, onRemove }) {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <Styledli>
-      <Styledtext>{text}</Styledtext>
+      <Styledtext onClick={() => setToggle(!toggle)} toggle={toggle}>
+        {text}
+      </Styledtext>
       <Styledremovebtn onClick={onRemove}>X</Styledremovebtn>
     </Styledli>
   );
@@ -12,6 +17,8 @@ export default function Item({ id, text, onRemove }) {
 
 const Styledli = styled.li``;
 
-const Styledtext = styled.span``;
+const Styledtext = styled.span`
+  text-decoration: ${(props) => (props.toggle ? "line-through" : "none")};
+`;
 
 const Styledremovebtn = styled.button``;
